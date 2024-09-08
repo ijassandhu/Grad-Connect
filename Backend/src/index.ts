d499c4f653e16ec663e04727f7d0f047bd721c2a
Jashanveer Singh <puniajashan85@gmail.com>
2024-09-08 17:58:58 +0530

    Started Building

---
+++ Backend/src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import connect from "./DBconn.ts"
+const app = express();
+
+connect();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (_req: Request, res: Response) => {
+  res.status(400).json({
+    success: true,
+    message: "Under Development👷",
+  });
+});
+
+app.listen(parseInt(process.env.PORT as string), "127.0.0.1", () => {
+  console.log(`Running at server at http://localhost:${process.env.PORT}`);
+});
