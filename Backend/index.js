dde770dac5a7ccf09a560855df1328e5a5adcb82
Jashanveer Singh <puniajashan85@gmail.com>
2024-09-12 00:23:58 +0530

    added eduInstituion and user Router

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import connect from "./DBconn.ts"
-import UserRouter from './routes/user.ts'
+import connect from "./DBconn.js";
+import UserRouter from "./routes/user.js";
+import eduInstitutionRouter from './routes/educationalIntitution.js'
 const app = express();
 
 connect();
@@ -10,15 +11,16 @@ connect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/user', UserRouter);
+app.use("/user", UserRouter);
+app.use('/eduInstitution', eduInstitutionRouter)
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/", (_req, res) => {
   res.status(400).json({
     success: true,
     message: "Under Development👷",
   });
 });
 
-app.listen(parseInt(process.env.PORT as string), "127.0.0.1", () => {
+app.listen(parseInt(process.env.PORT), "127.0.0.1", () => {
   console.log(`Running at server at http://localhost:${process.env.PORT}`);
 });
